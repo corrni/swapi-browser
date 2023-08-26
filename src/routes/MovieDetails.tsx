fb1091958f885afcd91f7baea3df7bff0d30db0d
Bruno Bakundwa <bruno.bakundwa@gmail.com>
2023-08-26 17:43:57 +0200

    Add characters to context when a movie page is loaded

--- src/routes/MovieDetails.tsx
+++ src/routes/MovieDetails.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import { compact } from 'lodash'
 
+import { useMovieCharacters } from '../context'
 import { fetchFilmById } from '../utils'
 
 const MovieDetails: React.FC = () => {
   const params = useParams<{ id: string }>()
   const { data } = useQuery(['films', params.id], () => fetchFilmById(params.id!))
+  const { addCharacters } = useMovieCharacters()
+
+  useEffect(() => {
+    const movieCharacters = compact(data?.characters)
+    addCharacters(movieCharacters)
+  }, [addCharacters, data?.characters])
 
   // KLUDGE: https://github.com/TanStack/query/issues/1297#issuecomment-730449036
   if (!data) {
