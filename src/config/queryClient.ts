6e27768468ddf22703fd451e78a3ee6f8fb6c88f
Bruno Bakundwa <bruno.bakundwa@gmail.com>
2023-08-31 15:04:30 +0200

    Reduce SWAPI re-fetches

--- src/config/queryClient.ts
+++ src/config/queryClient.ts
@@ -1,9 +1,14 @@
 import { QueryClient } from '@tanstack/react-query'
 
+// Should be safe to cache SWAPI data for 1 day
+const DEFAULT_CACHE_PERIOD = 1000 * 60 * 60 * 24
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       suspense: true,
+      staleTime: DEFAULT_CACHE_PERIOD,
+      cacheTime: DEFAULT_CACHE_PERIOD,
     },
   },
 })
